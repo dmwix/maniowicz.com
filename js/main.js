03ab3eda9cb812a3caa91b69b5e5cb6eae475f5d
dmwix <87998561+dmwix@users.noreply.github.com>
2022-02-19 00:00:11 -0300

    commiteo del sitio entero

---
+++ js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+function openNav() {
+    document.getElementById("fullscreen-menu").style.display = "block";
+}
+  
+function closeNav() {
+    document.getElementById("fullscreen-menu").style.display = "none";
+}
+  
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
