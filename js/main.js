0d8c40368509361bcd15652f3e02c7bf9b0c99a8
dmwix <87998561+dmwix@users.noreply.github.com>
2025-07-29 19:22:32 -0300

    cerrar accordion

--- js/main.js
+++ js/main.js
@@ -1,47 +1,50 @@
-'use strict';
+"use strict";
 
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
 function openNav() {
   document.getElementById("fullscreen-menu").style.display = "block";
 }
 
-const openMenu = document.getElementById('open-menu');
-openMenu.addEventListener('click', openNav);
+const openMenu = document.getElementById("open-menu");
+openMenu.addEventListener("click", openNav);
 
 function closeNav() {
   document.getElementById("fullscreen-menu").style.display = "none";
 }
 
-const closeMenu = document.getElementById('close-menu');
-closeMenu.addEventListener('click', closeNav);  
+const closeMenu = document.getElementById("close-menu");
+closeMenu.addEventListener("click", closeNav);
 
-let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+let acc = document.querySelectorAll(".accordion");
+acc.forEach((item) => {
+  item.addEventListener("click", function () {
     let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
+    let isOpen = panel.style.maxHeight ? true : false;
+
+    acc.forEach((a) => {
+      a.classList.remove("active");
+      a.nextElementSibling.style.maxHeight = null;
+    });
+
+    if (!isOpen) {
+      this.classList.add("active");
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
-}
+});
 
 let currentUrl = window.location.pathname;
 
-let navLinks = document.querySelectorAll('.desktop-menu ul a');
-navLinks.forEach(link => {
-  if(link.href.includes(`${currentUrl}`)) {
-    link.closest('li').classList.add('current-menu-item');
-  };
-})
+let navLinks = document.querySelectorAll(".desktop-menu ul a");
+navLinks.forEach((link) => {
+  if (link.href.includes(`${currentUrl}`)) {
+    link.closest("li").classList.add("current-menu-item");
+  }
+});
